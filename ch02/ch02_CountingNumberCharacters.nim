bba570b3a16493045274a09afeec522dd163a387
crossbow <crossbow@users.noreply.github.com>
2016-01-24 16:01:54 +0100

    Unicode friendly. Typographic enhancement.

--- ch02/ch02_CountingNumberCharacters.nim
+++ ch02/ch02_CountingNumberCharacters.nim
@@ -1,13 +1,10 @@
-## The answer is correct with ASCII string only
-## UNICODE version need to be designed
-
-import strutils
+import strutils, unicode
 
 proc prompt(msg: string = ">>> "): string =
   write(stdout, msg)
   return readLine(stdin)
 
 var inputString = prompt("What is the input string? ")
-var answer = "$1 has $2 characters." % [inputString, $len(inputString)]
+var answer = "\"$1\" has $2 characters." % [inputString, $runeLen(inputString)]
 
 echo answer
