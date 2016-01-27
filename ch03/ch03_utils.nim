632f83d89ecad358ad8c7611895b14f746211a40
crossbow <crossbow@users.noreply.github.com>
2016-01-27 22:36:12 +0100

    Initial commit

---
+++ ch03/ch03_utils.nim
@@ -0,0 +1,33 @@
+import strutils
+
+proc prompt*(msg: string = ">>> "): string =
+  write(stdout, msg)
+  return readLine(stdin)
+
+proc promptString*(msg: string = ">>> "): string =
+  write(stdout, msg)
+  result = readLine(stdin)
+  while result.isSpace() or result.len() <= 0 :
+    echo ""
+    write(stdout, msg)
+    result = readLine(stdin)
+
+proc promptInt*(msg: string = ">>> "): int =
+  ## Not negative
+  ## Not float
+  write(stdout, msg)
+  var res = readLine(stdin)
+  while not res.isDigit():
+    echo ""
+    write(stdout, msg)
+    res = readLine(stdin)
+  result = parseInt(res)
+
+when isMainModule:
+  echo "Testing testPromptString"
+  var testPromptString = promptString()
+  echo testPromptString
+
+  echo "Testing testPromptInt"
+  var testPromptInt = promptInt()
+  echo testPromptInt
