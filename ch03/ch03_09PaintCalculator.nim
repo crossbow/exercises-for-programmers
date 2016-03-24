641e3348fae8e056d9c7117f4980f0f79a71922c
crossbow <crossbow@users.noreply.github.com>
2016-03-24 18:15:49 +0100

    Initial commit

---
+++ ch03/ch03_09PaintCalculator.nim
@@ -0,0 +1,38 @@
+##[
+Calculate gallons of paint needed to paint the ceiling of a room. Prompt for
+the length and width, and assume one gallon covers 350 square feet. Display 
+the number of gallons needed to paint the ceiling as a whole number.
+
+Example Output
+​   
+You will need to purchase 2 gallons of paint to cover 360 square feet.
+
+Remember, you can’t buy a partial gallon of paint. You must round up to the 
+next whole gallon.
+
+Constraints
+    Use a constant to hold the conversion rate.
+    Ensure that you round up to the next whole number.
+]##
+
+import strutils
+import ch03_utils as utils
+
+## Constant
+const gallonPerSquareFeet = 350
+
+## Input
+var length = promptFloat("What is the length of the room in feet? ")
+var width = promptFloat("What is the width of the room in feet? ")
+
+## Processing
+var paint = 1
+var area = length * width
+var modulo = area.toInt() mod gallonPerSquareFeet
+if modulo == 0:
+  paint = area.toInt() div gallonPerSquareFeet
+else:
+  paint = paint + (area.toInt() div gallonPerSquareFeet)
+
+## Output
+echo "You will need to purchase $1 gallons of paint to cover $2 square feet" % [$paint, $area]
