6a2a5bdaeeebc0ba2ea6ad1c925a0e224450d2e5
crossbow <crossbow@users.noreply.github.com>
2016-03-25 07:57:09 +0100

    Get rid of strutils. Added strfmt for printfmt().

--- ch03/ch03_11CurrencyConverter.nim
+++ ch03/ch03_11CurrencyConverter.nim
@@ -4,7 +4,7 @@ dollars. Prompt for the amount of money in euros you have, and prompt for
 the current exchange rate of the euro. Print out the new amount in U.S. 
 dollars. The formula for currency conversion is
 
-amount_to = amount_from * rate_from
+    amount_to = amount_from * rate_from
 
 where
 
@@ -15,10 +15,9 @@ where
 Example Output
  
 How many euros are you exchanging? 81
-What is the exchange rate? 137.51
+What is the exchange rate? 1.3751
    
-81 euros at an exchange rate of 137.51 is   
-111.38 U.S. dollars.
+81 euros at an exchange rate of 1.3751 are 111.38 U.S. dollars.
 
 Constraints
 
@@ -29,7 +28,7 @@ For currency rate see:
 http://www.x-rates.com/calculator/?from=EUR&to=USD&amount=1
 ]##
 
-import strutils
+#import strutils
 import strfmt
 import ch03_utils as utils
 
@@ -41,6 +40,6 @@ var rateFrom = promptFloat("What is the exchange rate from euro to U.S. dollar? 
 var amountTo = amountFrom * rateFrom
 
 ## Output
-echo " "
+#echo " "
 #echo "$1 euros at an exchange rate of $2 are $3 U.S. dollars." % [$amountFrom, $rateFrom, $amountTo]
-printfmt("{:.2f} euros at an exchange rate of {:.5f} are {:.2f} U.S. dollars.\n", amountFrom, rateFrom, amountTo)
+printfmt("\n{:.2f} euros at an exchange rate of {:.4f} are {:.2f} U.S. dollars.\n", amountFrom, rateFrom, amountTo)
